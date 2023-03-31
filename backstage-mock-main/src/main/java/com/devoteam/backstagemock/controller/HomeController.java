3fe041e61911229c0c9c3f847f6e362cd6fc5d86
DVT-PW04N92S\Devoteam <Hijneke14@gmail.com>
2023-03-31 13:08:40 +0200

    added mock api for pipeline data

---
+++ backstage-mock-main/src/main/java/com/devoteam/backstagemock/controller/HomeController.java
@@ -0,0 +1,34 @@
+package com.devoteam.backstagemock.controller;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+@RestController
+@RequestMapping("/mock-data")
+public class HomeController {
+
+    @SneakyThrows
+    @GetMapping
+    public ResponseEntity<Object> getMockData() {
+
+        ClassPathResource staticDataResource = new ClassPathResource("mock-data.json");
+
+        String staticDataString = IOUtils.toString(staticDataResource.getInputStream(), StandardCharsets.UTF_8);
+
+        return ResponseEntity.ok(new JSONObject(staticDataString).toMap());
+
+    }
+
+}
