257df42b5694a79c0cfaee57976b997d2076015b
Kyle Watson <kyle.watson@devoteam.com>
2023-09-08 14:06:29 +0200

    Feature/#10 group sync env vars (#18)
    * #10: Adds a script to grab all groups from backstage, grabs teams csv from devlake, checks if teams are already added, adds team and uploads csv.
    Add readme for script,
    Add root readme.
    
    Still todo:
    - proper urls to apis
    - parented team structure
    
    * #10: Add project readmes to root readme, remove todos, remove prerequisites.
    
    * #10: Add env vars to change api urls.
    Add spaces for error message formatting to be more readable.
    
    * #10: Add REPLACE_DEVLAKE_TEAMS option to env vars

--- devlake-go/README.md
+++ devlake-go/README.md
@@ -6,7 +6,6 @@ This will retrieve all Backstage entities of type `Group` and insert the names a
 
 **Note:** group names are will be saved with the correct case in DevLake but group relationships are case insensitive. If you have groups: `groupa` and `GroupA`, one of them parenting `groupb`, `groupb` will be parented to the first group found in DevLake (order not guaranteed). I recommend to just make group names unique ignoring case.
 
-### TODO:
+By default, the script will look for DevLake at http://localhost:4000/ and Backstage at http://localhost:7007/ but this can be changed using environment variables, respectively: `BACKSTAGE_URL` and `DEVLAKE_URL`. **Make sure to include the trailing forward slash `/`**
 
-- Server URLs from environment variables
-- Error handling in case the teams table has not-yet been created
+If you wish to replace the DevLake teams table fully, you can set the environment variable `REPLACE_DEVLAKE_TEAMS` to any value. **Be careful:** This will delete all current DevLake teams.
--- devlake-go/group-sync.go
+++ devlake-go/group-sync.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"mime/multipart"
 	"net/http"
+	"os"
 	"slices"
 	"strconv"
 	"strings"
@@ -15,12 +16,19 @@ import (
 	"github.com/tdabasinskas/go-backstage/v2/backstage"
 )
 
+func lookupEnvDefault(envKey string, envDefaultValue string) string {
+	if val, ok := os.LookupEnv(envKey); ok {
+		return val
+	}
+	return envDefaultValue
+}
+
 const DevLakeTeamIdColumn = 0
 const DevLakeTeamNameColumn = 1
 const DevLakeTeamParentIdColumn = 3
 
 func retrieveBackstageTeams() []backstage.Entity {
-	client, err := backstage.NewClient("http://localhost:7007/", "default", nil)
+	client, err := backstage.NewClient(lookupEnvDefault("BACKSTAGE_URL", "http://localhost:7007/"), "default", nil)
 	backstageTeams, _, err := client.Catalog.Entities.List(context.Background(), &backstage.ListEntityOptions{
 		Filters: []string{
 			"kind=group",
@@ -30,21 +38,29 @@ func retrieveBackstageTeams() []backstage.Entity {
 	})
 
 	if err != nil {
-		log.Fatal("Cannot retrieve Backstage teams", err)
+		log.Fatal("Cannot retrieve Backstage teams: ", err)
 	}
 
 	return backstageTeams
 }
 
+func devLakeTeamsApiUrlFromEnv() string {
+	return lookupEnvDefault("DEVLAKE_URL", "http://localhost:4000/") + "api/plugins/org/teams.csv"
+}
+
 func retrieveDevLakeTeams() [][]string {
-	resp, err := http.Get("http://localhost:4000/api/plugins/org/teams.csv")
+	if _, ok := os.LookupEnv("REPLACE_DEVLAKE_TEAMS"); ok {
+		return [][]string{{"Id", "Name", "Alias", "ParentId", "SortingIndex"}}
+	}
+
+	resp, err := http.Get(devLakeTeamsApiUrlFromEnv())
 	if err != nil {
-		log.Fatal("Cannot retrieve DevLake teams", err)
+		log.Fatal("Cannot retrieve DevLake teams: ", err)
 	}
 	csvReader := csv.NewReader(resp.Body)
 	devLakeTeams, err := csvReader.ReadAll()
 	if err != nil {
-		log.Fatal("Cannot read DevLake team CSV format", err)
+		log.Fatal("Cannot read DevLake team CSV format: ", err)
 	}
 
 	return devLakeTeams
@@ -106,7 +122,7 @@ func updateDevLakeTeams(devLakeTeams [][]string) {
 	csvWriter := csv.NewWriter(buf)
 
 	if err := csvWriter.WriteAll(devLakeTeams); err != nil {
-		log.Fatal("Cannot write DevLake teams to CSV format", err)
+		log.Fatal("Cannot write DevLake teams to CSV format: ", err)
 	}
 
 	multipartBody := &bytes.Buffer{}
@@ -121,10 +137,10 @@ func updateDevLakeTeams(devLakeTeams [][]string) {
 		log.Fatal("Cannot close CSV writer: ", err)
 	}
 
-	req, err := http.NewRequest("PUT", "http://localhost:4000/api/plugins/org/teams.csv", multipartBody)
+	req, err := http.NewRequest("PUT", devLakeTeamsApiUrlFromEnv(), multipartBody)
 
 	if err != nil {
-		log.Fatal("Cannot create DevLake PUT request", err)
+		log.Fatal("Cannot create DevLake PUT request: ", err)
 	}
 
 	req.Header.Add("Content-Type", writer.FormDataContentType())
@@ -133,11 +149,11 @@ func updateDevLakeTeams(devLakeTeams [][]string) {
 	resp, err := httpClient.Do(req)
 
 	if err != nil {
-		log.Fatal("Cannot update DevLake teams CSV", err)
+		log.Fatal("Cannot update DevLake teams CSV: ", err)
 	}
 	resBody, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatal("Cannot read response from DevLake teams update request", err)
+		log.Fatal("Cannot read response from DevLake teams update request: ", err)
 	}
 
 	log.Printf("Response: %s\n", resBody)
