6c0f973625c4d7556157e10844fff79cd662b535
Kyle Watson <kyle.watson@devoteam.com>
2023-09-11 14:24:26 +0200

    #10: Refactor devlake teams downloader to return errors to allow testing unhappy paths.

--- devlake-go/cmd/group-sync/main.go
+++ devlake-go/cmd/group-sync/main.go
@@ -4,13 +4,17 @@ import (
 	"devlake-go/group-sync/pkg/backstage"
 	"devlake-go/group-sync/pkg/conversion"
 	"devlake-go/group-sync/pkg/devlake"
+	"log"
 )
 
 func main() {
 	backstageTeams := backstage.RetrieveTeams()
 
 	devlakeApiUrl := devlake.TeamsApiUrlFromEnv()
-	devLakeTeams := devlake.RetrieveTeams(devlakeApiUrl)
+	devLakeTeams, err := devlake.RetrieveTeams(devlakeApiUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
 	devLakeTeams = conversion.BackstageTeamsToDevLakeTeams(backstageTeams, devLakeTeams)
 
 	devlake.UpdateTeams(devlakeApiUrl, devLakeTeams)
--- devlake-go/pkg/devlake/downloader_test.go
+++ devlake-go/pkg/devlake/downloader_test.go
@@ -19,10 +19,44 @@ func TestRetrieveTeams(t *testing.T) {
 
 	defer testServer.Close()
 
-	csv := RetrieveTeams(testServer.URL)
+	csv, err := RetrieveTeams(testServer.URL)
+	if err != nil {
+		t.Fatalf("unexpected error retrieving teams: %v", err)
+	}
 	want := test.ExampleCsvWithColumnHeaders([][]string{{"1", "Maple Leafs", "ML", "2", "0"}, {"2", "Friendly Confines", "FC", "", "1"}, {"3", "Blue Jays", "BJ", "", "2"}})
 
 	if !reflect.DeepEqual(csv, want) {
 		t.Errorf("got %v, want %v", csv, want)
+	}
+}
+
+func TestReplaceTeams(t *testing.T) {
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/api/plugins/org/teams.csv" {
+			t.Errorf("Expected to request '/api/plugins/org/teams.csv'/, got: %s", r.URL.Path)
+		}
+		fmt.Fprintln(w, "Id,Name,Alias,ParentId,SortingIndex\n1,Maple Leafs,ML,2,0\n2,Friendly Confines,FC,,1\n3,Blue Jays,BJ,,2")
+	}))
+
+	defer testServer.Close()
+
+	t.Setenv("REPLACE_DEVLAKE_TEAMS", "true")
+
+	csv, err := RetrieveTeams(testServer.URL)
+	if err != nil {
+		t.Fatalf("unexpected error retrieving teams: %v", err)
+	}
+	want := test.ExampleCsvWithColumnHeaders([][]string{})
+
+	if !reflect.DeepEqual(csv, want) {
+		t.Errorf("got %v, want %v", csv, want)
+	}
+}
+
+func TestNoServer(t *testing.T) {
+	csv, err := RetrieveTeams("http://localhost/no-server")
+
+	if err == nil || csv != nil {
+		t.Errorf("Expected no connection to the server to return an error, got: %v", csv)
 	}
 }
