93f22608f3a187e09dd959700895f6a51f0c3542
Kyle Watson <kyle.watson@devoteam.com>
2023-09-18 17:20:30 +0200

    #26: Fix tests with newlines, actually validate response

--- devlake-go/api/main_test.go
+++ devlake-go/api/main_test.go
@@ -27,21 +27,26 @@ func Test_metricHandler(t *testing.T) {
 		{
 			name:       "should throw 400 response when not specifying metric type",
 			req:        httptest.NewRequest(http.MethodGet, "/dora/api/metric?", nil),
-			expectBody: "type should be provided as either df_average or df_total",
+			expectBody: "type should be provided as either df_average or df_total\n",
 		},
 		{
 			name:       "should throw 400 response when specifying nonsense metric type",
 			req:        httptest.NewRequest(http.MethodGet, "/dora/api/metric?type=not_metric", nil),
-			expectBody: "type should be provided as either df_average or df_total",
+			expectBody: "type should be provided as either df_average or df_total\n",
+		},
+		{
+			name:       "should throw 400 response when specifying multiple metric types",
+			req:        httptest.NewRequest(http.MethodGet, "/dora/api/metric?type=df_total&type=df_average", nil),
+			expectBody: "type should be provided as either df_average or df_total\n",
 		},
 		{
 			name:       "should return data response when specifying df_total",
-			req:        httptest.NewRequest(http.MethodGet, "/dora/api/metric?type=df_total", nil),
-			expectBody: `{"aggregation":"weekly","dataPoints":[]}`,
+			req:        httptest.NewRequest(http.MethodGet, "/dora/api/metric?project=my-project&type=df_total", nil),
+			expectBody: `{"aggregation":"weekly","dataPoints":[]}` + "\n",
 		},
 		{
 			name:       "should return todo response when specifying df_average",
-			req:        httptest.NewRequest(http.MethodGet, "/dora/api/metric?type=df_total", nil),
+			req:        httptest.NewRequest(http.MethodGet, "/dora/api/metric?type=df_average", nil),
 			expectBody: "todo average",
 		},
 	}
@@ -57,8 +62,8 @@ func Test_metricHandler(t *testing.T) {
 			if err != nil {
 				t.Errorf("expected error to be nil got %v", err)
 			}
-			if string(data) == tt.expectBody {
-				t.Errorf("expected %v got %v", tt.expectBody, string(data))
+			if string(data) != tt.expectBody {
+				t.Errorf("expected '%v' got '%v'", tt.expectBody, string(data))
 			}
 		})
 	}
