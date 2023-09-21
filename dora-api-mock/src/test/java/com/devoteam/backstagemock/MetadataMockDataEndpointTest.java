50faa3861a4056b3ac32dcd49bee2d25ba4b6a02
Bruno Ribeiro <bruno@brunocesar.com>
2023-09-21 14:56:10 +0200

    feature: #14 [BE] Expose Deployment Frequency mocked endpoint (#35)
    - add stubs for the `df_count` metric type

--- dora-api-mock/src/test/java/com/devoteam/backstagemock/MetadataMockDataEndpointTest.java
+++ dora-api-mock/src/test/java/com/devoteam/backstagemock/MetadataMockDataEndpointTest.java
@@ -3,15 +3,21 @@ package com.devoteam.backstagemock;
 import com.jayway.jsonpath.Configuration;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.ActiveProfiles;
 
 import static com.jayway.jsonpath.matchers.JsonPathMatchers.hasJsonPath;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
+@ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class MetadataMockDataEndpointTest {
+
+    @Value("${wiremock.server.port}")
+    private int wireMockPort;
 
     @Autowired
     private TestRestTemplate restTemplate;
@@ -49,7 +55,7 @@ class MetadataMockDataEndpointTest {
                 hasKey("web_url")
         );
 
-        var result = restTemplate.getForObject("http://localhost:10666/mock-data", String.class);
+        var result = restTemplate.getForObject("http://localhost:%s/mock-data".formatted(wireMockPort), String.class);
 
         var json = Configuration.defaultConfiguration().jsonProvider().parse(result);
         assertThat(json, hasJsonPath("$.metadata_1", is(metadataMatcher)));
