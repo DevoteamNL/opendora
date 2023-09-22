9b107307aaac63ac4adb0c636775d5570370b522
Kyle Watson <kyle.watson@devoteam.com>
2023-09-22 13:45:56 +0200

    #13: Use the openapi specified url to get data (works with mock and real). Specify the team and time parameters in URL. Add cors option to wiremock properties to allow cross-origin requests. Set the time unit values to lower case to match the open api spec.

--- backstage-plugin/plugins/dora-plugin/src/services/GroupDataService.ts
+++ backstage-plugin/plugins/dora-plugin/src/services/GroupDataService.ts
@@ -1,12 +1,11 @@
 import { DeploymentFrequencyData } from '../models/DeploymentFrequencyData';
 
 export const getMockData = async (
-  // TODO #13: Use these parameters in fetch and remove underscore
-  _groupQueryParam: string,
-  _selectedTimeUnit: string,
+  groupQueryParam: string,
+  selectedTimeUnit: string,
 ) => {
   try {
-    const data = await fetch('http://localhost:8080/mock-data', {
+    const data = await fetch(`http://localhost:10666/dora/api/metric?type=df_count&aggregation=${selectedTimeUnit}&team=${groupQueryParam}`, {
       method: 'GET',
     });
     return (await data.json()) as DeploymentFrequencyData;
