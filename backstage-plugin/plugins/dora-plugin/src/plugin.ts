4df09be26ab31b8b1238b9f965f1888f15d42d17
Kyle Watson <kyle.watson@devoteam.com>
2023-09-26 10:59:56 +0200

    #13: Adds a config option to specify the backend api url. (#39)

--- backstage-plugin/plugins/dora-plugin/src/plugin.ts
+++ backstage-plugin/plugins/dora-plugin/src/plugin.ts
@@ -1,14 +1,27 @@
 import {
+  configApiRef,
+  createApiFactory,
   createPlugin,
   createRoutableExtension,
 } from '@backstage/core-plugin-api';
 import { rootRouteRef } from './routes';
+import {
+  GroupDataService,
+  groupDataServiceApiRef,
+} from './services/GroupDataService';
 
 export const doraPluginPlugin = createPlugin({
   id: 'dora-plugin',
   routes: {
     root: rootRouteRef,
   },
+  apis: [
+    createApiFactory({
+      api: groupDataServiceApiRef,
+      deps: { configApi: configApiRef },
+      factory: ({ configApi }) => new GroupDataService({ configApi }),
+    }),
+  ],
 });
 
 export const DoraPluginPage = doraPluginPlugin.provide(
