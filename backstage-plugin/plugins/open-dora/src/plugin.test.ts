8a064c0d1ec5ef1200a3f9988aa5728d2861e379
Kyle Watson <kyle.watson@devoteam.com>
2023-10-24 14:20:07 +0200

    #68: Add test for plugin page

--- backstage-plugin/plugins/open-dora/src/plugin.test.ts
+++ backstage-plugin/plugins/open-dora/src/plugin.test.ts
@@ -1,7 +1,11 @@
-import { openDoraPlugin } from './plugin';
+import { OpenDoraPluginPage, openDoraPlugin } from './plugin';
 
 describe('dora-plugin', () => {
   it('should export plugin', () => {
     expect(openDoraPlugin).toBeDefined();
+  });
+
+  it('should export plugin page', () => {
+    expect(OpenDoraPluginPage).toBeDefined();
   });
 });
