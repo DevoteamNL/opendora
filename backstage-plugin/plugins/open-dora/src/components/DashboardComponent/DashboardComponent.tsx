d473a226db38f6629f1acdf81dfe3a51d0fa6c6f
Damjan Jelic <103260312+duke-b@users.noreply.github.com>
2023-11-14 15:58:53 +0100

    Added tests for average (#104)
    * Added tests for average
    
    * cleared an unused import
    
    * Added new error handling
    
    * Removed extra comment
    
    * #12 - Adjusted tests for server error
    
    * #12 fixed import error

--- backstage-plugin/plugins/open-dora/src/components/DashboardComponent/DashboardComponent.tsx
+++ backstage-plugin/plugins/open-dora/src/components/DashboardComponent/DashboardComponent.tsx
@@ -9,26 +9,41 @@ import {
 import { useApi } from '@backstage/core-plugin-api';
 import { getEntityRelations, useEntity } from '@backstage/plugin-catalog-react';
 import { Grid } from '@material-ui/core';
-import React, { useEffect } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { MetricData } from '../../models/MetricData';
 import { groupDataServiceApiRef } from '../../services/GroupDataService';
 import { BarChartComponent } from '../BarChartComponent/BarChartComponent';
 import { DropdownComponent } from '../DropdownComponent/DropdownComponent';
 import './DashboardComponent.css';
+import { ChartErrors } from '../../models/CustomErrors';
 
 export interface DashboardComponentProps {
   entityName?: string;
   entityGroup?: string;
 }
+
+function dataErrorReducer(
+  currentErrors: ChartErrors,
+  action: { type: keyof ChartErrors; error: Error },
+) {
+  return {
+    ...currentErrors,
+    [action.type]: action.error,
+  };
+}
+
 export const DashboardComponent = ({
   entityName,
   entityGroup,
 }: DashboardComponentProps) => {
   const [chartData, setChartData] = React.useState<MetricData | null>(null);
+  const [chartDataAverage, setChartDataAverage] =
+    React.useState<MetricData | null>(null);
   const [selectedTimeUnit, setSelectedTimeUnit] = React.useState('weekly');
-  const [dataError, setDataError] = React.useState<Error | undefined>(
-    undefined,
-  );
+
+  const initialErrors: ChartErrors = { countError: null, averageError: null };
+
+  const [dataError, dispatch] = useReducer(dataErrorReducer, initialErrors);
 
   const groupDataService = useApi(groupDataServiceApiRef);
 
@@ -45,13 +60,41 @@ export const DashboardComponent = ({
           setChartData(response);
         },
         (error: Error) => {
-          setDataError(error);
+          dispatch({
+            type: 'countError',
+            error: error,
+          });
+        },
+      );
+
+    groupDataService
+      .retrieveMetricDataPoints({
+        type: 'df_average',
+        team: entityGroup,
+        aggregation: selectedTimeUnit,
+        project: entityName,
+      })
+      .then(
+        response => {
+          setChartDataAverage(response);
+        },
+        (error: Error) => {
+          dispatch({
+            type: 'averageError',
+            error: error,
+          });
         },
       );
   }, [entityGroup, entityName, selectedTimeUnit, groupDataService]);
 
   const chartOrProgressComponent = chartData ? (
     <BarChartComponent metricData={chartData} />
+  ) : (
+    <Progress variant="indeterminate" />
+  );
+
+  const chartOrProgressComponentAverage = chartDataAverage ? (
+    <BarChartComponent metricData={chartDataAverage} />
   ) : (
     <Progress variant="indeterminate" />
   );
@@ -83,10 +126,20 @@ export const DashboardComponent = ({
             <Grid item xs={12} className="gridBorder">
               <div className="gridBoxText">
                 <h1>Deployment Frequency</h1>
-                {dataError ? (
-                  <ResponseErrorPanel error={dataError} />
+                {dataError.countError ? (
+                  <ResponseErrorPanel error={dataError.countError} />
                 ) : (
                   chartOrProgressComponent
+                )}
+              </div>
+            </Grid>
+            <Grid item xs={12} className="gridBorder">
+              <div className="gridBoxText">
+                <h1>Deployment Frequency Average</h1>
+                {dataError.averageError ? (
+                  <ResponseErrorPanel error={dataError.averageError} />
+                ) : (
+                  chartOrProgressComponentAverage
                 )}
               </div>
             </Grid>
