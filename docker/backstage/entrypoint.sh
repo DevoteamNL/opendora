d4918e11a10eb2178f4053c196fd2bbbff653212
Jeroen van der Meulen <jeroennathan.vandermeulen@gmail.com>
2024-03-14 11:37:45 +0100

    Make backstage-plugin useable for dev

--- docker/backstage/entrypoint.sh
+++ docker/backstage/entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-[ -z "$(ls -A /app)" ] && yes app | npx @backstage/create-app@latest --path /app
+[ -z "$(ls -A /app/node_modules)" ] && yarn install
 
-cp app-config.yaml /app
-cp App.tsx /app/packages/app/src/App.tsx
-cp Root.tsx /app/packages/app/src/components/Root/Root.tsx
-
-cd /app && yarn --cwd packages/app add @devoteam-nl/open-dora-backstage-plugin && yarn install && yarn tsc && yarn --cwd packages/backend build && yarn dev
+yarn dev
